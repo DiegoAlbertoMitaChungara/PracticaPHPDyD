04f92a5abfbc3501e1be3fb0ba2108d15d2ddf71
DarkDiegoMC <dmita@ucm.es>
2025-03-19 22:20:31 +0100

    Bootstrap implementado a pagina2.php

--- pagina2.php
+++ pagina2.php
@@ -4,6 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        #titulo{
+            height: 150px;
+            border: 2px solid black;
+            border-radius: 15px;
+            text-align: center;
+            align-content: center;
+        }
+        #formulario{
+            padding: 2%;
+            border: 2px solid black;
+            border-radius: 15px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
 </head>
 <?php
     session_start();
@@ -316,108 +335,148 @@
 
 ?>
 <body>
-    <h1>Clases de Dungeons & Dragons</h1>
-    <?php
-        echo "Ahora que conocemos tu origen ".$_SESSION["razaPersonaje"].", ".$_SESSION["nombrePersonaje"].", es hora de averiguar qué clase es la idónea para una futura leyenda como tú.";
-    ?>
 
-    <p>En Dungeons & Dragons existen diversas clases únicas con sus respectivas armas. Algunas de las principales son:</p>
-    <p>
-        <strong>Bárbaro:</strong> Guerrero feroz que canaliza su furia y fuerza bruta en el combate, destacando en la agresividad y el poder físico.
-        <br>
-        <span style="color: gray; font-weight: bold;">Hacha de batalla: </span> Pesada y brutal, el hacha de batalla es el arma perfecta para un guerrero salvaje que se lanza al combate con furia imparable.
-        <br>
-    </p>
-    <p>
-        <strong>Bardo:</strong> Artista encantador que usa la música, la poesía y habilidades mágicas para inspirar y manipular a aliados y adversarios.
-        <br>
-        <span style="color: gray; font-weight: bold;">Estoque y laúd: </span> Un estoque elegante, usado con destreza en duelos. Combinado con su laúd, el bardo lucha tanto con la música como con la espada.
-        <br>
-    </p>
-    <p>
-        <strong>Brujo:</strong> Portador de magia oscura adquirida a través de pactos con entidades misteriosas, su poder es tan enigmático como peligroso.
-        <br>
-        <span style="color: gray; font-weight: bold;">Báculo arcano: </span> Un bastón imbuido con poder oscuro, utilizado para canalizar la magia obtenida de pactos con entidades místicas.
-        <br>
-    </p>
-    <p>
-        <strong>Clérigo:</strong> Devoto de una deidad, que utiliza la fe y el poder divino para sanar, proteger y luchar contra el mal.
-        <br>
-        <span style="color: gray; font-weight: bold;">Maza: </span> Un arma contundente utilizada por los servidores de los dioses, diseñada para aplastar el mal sin derramar sangre.
-        <br>
-    </p>
-    <p>
-        <strong>Druida:</strong> Guardián de la naturaleza que convoca el poder de los elementos, protegiendo el equilibrio natural del mundo.
-        <br>
-        <span style="color: gray; font-weight: bold;">Bastón de madera: </span> Un símbolo de su conexión con la naturaleza, este bastón les ayuda a lanzar conjuros y controlar los elementos.
-        <br>
-    </p>
-    <p>
-        <strong>Explorador:</strong> Experto en rastreo, sigilo y supervivencia, ideal para emboscadas y para navegar terrenos inhóspitos.
-        <br>
-        <span style="color: gray; font-weight: bold;">Arco largo: </span> Un arma de gran alcance, perfecta para emboscadas y ataques a distancia. Utilizado por cazadores y supervivientes que dominan los terrenos salvajes.
-        <br>
-    </p>
-    <p>
-        <strong>Guerrero:</strong> Combatiente versátil y disciplinado, entrenado en múltiples estilos de lucha y armado para enfrentar cualquier desafío.
-        <br>
-        <span style="color: gray; font-weight: bold;">Espada bastarda: </span> También conocida como "espada de mano y media", es versátil y poderosa, permitiendo ataques letales tanto con una como con ambas manos.
-        <br>
-    </p>
-    <p>
-        <strong>Hechicero:</strong> Usuario de magia innata, cuyo poder surge de su propia esencia, manifestándose de forma explosiva y natural.
-        <br>
-        <span style="color: gray; font-weight: bold;">Varita mágica: </span> Un canalizador de poder que amplifica la magia innata del hechicero, permitiéndole desatar conjuros con facilidad.
-        <br>
-    </p>
-    <p>
-        <strong>Mago:</strong> Erudito del conocimiento arcano, capaz de aprender y lanzar una amplia variedad de hechizos a través del estudio riguroso.
-        <br>
-        <span style="color: gray; font-weight: bold;">Libro de hechizos y daga: </span> El libro contiene el verdadero arsenal de un mago: su conocimiento de la magia. La daga es una herramienta de emergencia, más que un arma principal.
-        <br>
-    </p>
-    <p>
-        <strong>Monje:</strong> Maestro de la disciplina física y mental, que combina artes marciales y meditación para lograr un equilibrio perfecto.
-        <br>
-        <span style="color: gray; font-weight: bold;">Puños: </span> A través del entrenamiento marcial, el monje convierte su propio cuerpo en un arma mortal, capaz de derribar enemigos con golpes rápidos y precisos.
-        <br>
-    </p>
-    <p>
-        <strong>Paladín:</strong> Caballero sagrado comprometido con la justicia, que canaliza habilidades divinas para proteger a los inocentes y combatir el mal.
-        <br>
-        <span style="color: gray; font-weight: bold;">Espada larga: </span> Una espada de hoja recta, ideal para el combate con una sola mano o a dos manos. Simboliza honor, justicia y la lucha contra el mal.
-        <br>
-    </p>
-    <p>
-        <strong>Pícaro:</strong> Especialista en el sigilo, la astucia y los trucos, ideal para emboscadas y para aprovechar la precisión en el combate.
-        <br>
-        <span style="color: gray; font-weight: bold;">Daga: </span> Pequeña, rápida y letal en manos expertas. Perfecta para ataques sorpresa y combates en la oscuridad.
-    </p>
+    <div class="row">
+        <div class="col-3"></div>
+            <div class="col-6" id="titulo">
+                <h1>Clases de Dungeons & Dragons</h1>
+            </div>
+        <div class="col-3"></div>
+    </div>
+
+    <div class="row">
+        <div class="col-3">
+            
+
+        </div>
+        <div class="col-6" id="formulario">
+            <p><?php echo $_SESSION["nombrePersonaje"].", "?>ahora que conocemos tu origen: <?php echo $_SESSION["razaPersonaje"]?>, es hora de averiguar qué clase es la idónea para una futura leyenda como tú.</p>
+            <p>En Dungeons & Dragons existen diversas clases únicas con sus respectivas armas. Algunas de las principales son:</p>
+
+            <div id="descripcionClases">
+                <p>
+                    <strong>Bárbaro:</strong> Guerrero feroz que canaliza su furia y fuerza bruta en el combate, destacando en la agresividad y el poder físico.
+                    <br>
+                    <span style="color: gray; font-weight: bold;">Hacha de batalla: </span> Pesada y brutal, el hacha de batalla es el arma perfecta para un guerrero salvaje que se lanza al combate con furia imparable.
+                    <br>
+                </p>
+
+                <p>
+                    <strong>Bardo:</strong> Artista encantador que usa la música, la poesía y habilidades mágicas para inspirar y manipular a aliados y adversarios.
+                    <br>
+                    <span style="color: gray; font-weight: bold;">Estoque y laúd: </span> Un estoque elegante, usado con destreza en duelos. Combinado con su laúd, el bardo lucha tanto con la música como con la espada.
+                    <br>
+                </p>
+                
+                <p>
+                    <strong>Brujo:</strong> Portador de magia oscura adquirida a través de pactos con entidades misteriosas, su poder es tan enigmático como peligroso.
+                    <br>
+                    <span style="color: gray; font-weight: bold;">Báculo arcano: </span> Un bastón imbuido con poder oscuro, utilizado para canalizar la magia obtenida de pactos con entidades místicas.
+                    <br>
+                </p>
+
+                <p>
+                    <strong>Clérigo:</strong> Devoto de una deidad, que utiliza la fe y el poder divino para sanar, proteger y luchar contra el mal.
+                    <br>
+                    <span style="color: gray; font-weight: bold;">Maza: </span> Un arma contundente utilizada por los servidores de los dioses, diseñada para aplastar el mal sin derramar sangre.
+                    <br>
+                </p>
+
+                <p>
+                    <strong>Druida:</strong> Guardián de la naturaleza que convoca el poder de los elementos, protegiendo el equilibrio natural del mundo.
+                    <br>
+                    <span style="color: gray; font-weight: bold;">Bastón de madera: </span> Un símbolo de su conexión con la naturaleza, este bastón les ayuda a lanzar conjuros y controlar los elementos.
+                    <br>
+                </p>
+
+                <p>
+                    <strong>Explorador:</strong> Experto en rastreo, sigilo y supervivencia, ideal para emboscadas y para navegar terrenos inhóspitos.
+                    <br>
+                    <span style="color: gray; font-weight: bold;">Arco largo: </span> Un arma de gran alcance, perfecta para emboscadas y ataques a distancia. Utilizado por cazadores y supervivientes que dominan los terrenos salvajes.
+                    <br>
+                </p>
+
+                <p>
+                    <strong>Guerrero:</strong> Combatiente versátil y disciplinado, entrenado en múltiples estilos de lucha y armado para enfrentar cualquier desafío.
+                    <br>
+                    <span style="color: gray; font-weight: bold;">Espada bastarda: </span> También conocida como "espada de mano y media", es versátil y poderosa, permitiendo ataques letales tanto con una como con ambas manos.
+                    <br>
+                </p>
+                
+                <p>
+                    <strong>Hechicero:</strong> Usuario de magia innata, cuyo poder surge de su propia esencia, manifestándose de forma explosiva y natural.
+                    <br>
+                    <span style="color: gray; font-weight: bold;">Varita mágica: </span> Un canalizador de poder que amplifica la magia innata del hechicero, permitiéndole desatar conjuros con facilidad.
+                    <br>
+                </p>
+
+                <p>
+                    <strong>Mago:</strong> Erudito del conocimiento arcano, capaz de aprender y lanzar una amplia variedad de hechizos a través del estudio riguroso.
+                    <br>
+                    <span style="color: gray; font-weight: bold;">Libro de hechizos y daga: </span> El libro contiene el verdadero arsenal de un mago: su conocimiento de la magia. La daga es una herramienta de emergencia, más que un arma principal.
+                    <br>
+                </p>
+
+                <p>
+                    <strong>Monje:</strong> Maestro de la disciplina física y mental, que combina artes marciales y meditación para lograr un equilibrio perfecto.
+                    <br>
+                    <span style="color: gray; font-weight: bold;">Puños: </span> A través del entrenamiento marcial, el monje convierte su propio cuerpo en un arma mortal, capaz de derribar enemigos con golpes rápidos y precisos.
+                    <br>
+                </p>
+
+                <p>
+                    <strong>Paladín:</strong> Caballero sagrado comprometido con la justicia, que canaliza habilidades divinas para proteger a los inocentes y combatir el mal.
+                    <br>
+                    <span style="color: gray; font-weight: bold;">Espada larga: </span> Una espada de hoja recta, ideal para el combate con una sola mano o a dos manos. Simboliza honor, justicia y la lucha contra el mal.
+                    <br>
+                </p>
+
+                <p>
+                    <strong>Pícaro:</strong> Especialista en el sigilo, la astucia y los trucos, ideal para emboscadas y para aprovechar la precisión en el combate.
+                    <br>
+                    <span style="color: gray; font-weight: bold;">Daga: </span> Pequeña, rápida y letal en manos expertas. Perfecta para ataques sorpresa y combates en la oscuridad.
+                </p>
+
+            </div>
+            
+
+            <form action="pagina3.php" method="POST">
+                <!-- Pregunta 1: Number -->
+                <div class="mb-3">
+                    <label>¿A cuántos enemigos preferirías enfrentar en combate, <?php echo $_SESSION["nombrePersonaje"]." el ".$_SESSION["razaPersonaje"] ?>?</label>
+                    <input type="number" name="pregunta1C" min="1" max="30" value="1">
+                    <div class="form-text">Mínimo 1, máximo 30</div>
+                </div>
+                <!-- Pregunta 2: Color -->
+                <div class="mb-3">
+                    <label>Elige el color con el que te sientas más identificado, <?php echo $_SESSION["nombrePersonaje"]." el ".$_SESSION["razaPersonaje"] ?>?</label>
+                    <input type="color" name="pregunta2C">
+                </div>
+                <!-- Pregunta 3: Range -->
+                <div class="mb-3">
+                    <label>¿Cuántas aliados prefieres tener a tu lado en una batalla decisiva, <?php echo $_SESSION["nombrePersonaje"]." el ".$_SESSION["razaPersonaje"] ?>?</label>
+                    <input type="range" class="form-range" name="pregunta3C" min="0" max="25">
+                    <div class="form-text">Mínimo 0, máximo 25</div>
+                </div>
+                <!-- Pregunta 4: Date -->
+                <div class="mb-3">
+                    <label>¿En qué fecha iniciarías tu aventura épica, <?php echo $_SESSION["nombrePersonaje"]." el ".$_SESSION["razaPersonaje"] ?>?</label>
+                    <input type="date" name="pregunta4C" min="2025-01-01" max="2025-12-31" value="2025-01-01">
+                    <div class="form-text">Solo fechas de 2025</div>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
 
 
-    <form action="pagina3.php" method="POST">
-
-        <!-- Pregunta 1 -->
-        <p>¿A cuántos enemigos preferirías enfrentar en combate, <?php echo $_SESSION["nombrePersonaje"]." el ".$_SESSION["razaPersonaje"] ?>?</p>
-        <input type="number" name="pregunta1C" min="1" max="30" value="1">
-
-        <!-- Pregunta 2 -->
-        <p>Elige el color con el que te sientas más identificado, <?php echo $_SESSION["nombrePersonaje"]." el ".$_SESSION["razaPersonaje"] ?>?</p>
-        <input type="color" name="pregunta2C">
-        
-        <!-- Pregunta 3 -->
-        <p>¿Cuántas aliados prefieres tener a tu lado en una batalla decisiva, <?php echo $_SESSION["nombrePersonaje"]." el ".$_SESSION["razaPersonaje"] ?>? (0-25)</p>
-        <input type="range" name="pregunta3C" min="0" max="25" style="width: 500px;">
-
-        <!-- Pregunta 4 -->
-        <p>¿En qué fecha iniciarías tu aventura épica, <?php echo $_SESSION["nombrePersonaje"]." el ".$_SESSION["razaPersonaje"] ?>? (solo se evalúa el mes y el día)</p>
-        <input type="date" name="pregunta4C" min="2025-01-01" max="2025-12-31" value="2025-01-01">
-
-        <br><br>
-        <input type="submit" value="Conocer Clase">
+        </div>
+        <div class="col-3">
 
 
-    </form>
+        </div>
+    </div>
+    
+
+    
     
 </body>
 </html>
